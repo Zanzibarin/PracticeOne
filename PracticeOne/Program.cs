1b1fee4b6afd524c96c1df5fcf8ac6db00d5f6f5
Zanzibarin <alex_potemkin@mail.ru>
2021-11-08 23:20:27 +0300

    Add files via upload

---
+++ PracticeOne/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PracticeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (isSimple(i))
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+        }
+        
+        private static bool isSimple(int N) //метод, определяющий, простое число, или нет
+        {            
+            for (int i = 2; i <= (int)(N / 2); i++) //чтобы убедится, простое число, или нет, достаточно проверить, не делитсья ли число на числа до его половины
+            {
+                if (N % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+
+}
